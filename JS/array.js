ebc22b1c80544e1ef6c47d0825ed3df6651273f0
arunavan <arunajava567@gmail.com>
2023-02-08 14:04:25 +0530

    Add files via upload

---
+++ JS/array.js
@@ -0,0 +1,10 @@
+
+var a=["java","spring","hibernate"];
+
+var b=new Array("js","css","html","angular");
+for(var i=0;i<a.length;i++) {
+    console.log(a[i]);
+}
+for(var i=0;i<b.length;i++) {
+    console.log(b[i]);
+}
